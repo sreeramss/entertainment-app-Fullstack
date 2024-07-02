7d7d38c40052af69e85e75acc85186e59c05e125
Sree Ram  S S <89720320+sreeramss@users.noreply.github.com>
2024-07-02 23:12:10 +0530

    Delete Backend directory

--- Backend/src/components/Logout.jsx
+++
@@ -1,9 +0,0 @@
-// Function to handle user logout
-export const logout = (navigate) => {
-  // Remove the token from local storage
-  localStorage.removeItem("token");
-  // Remove the user ID from local storage
-  localStorage.removeItem("userId");
-  // Navigate to the home page
-  navigate("/");
-};
